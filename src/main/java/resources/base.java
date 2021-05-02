f452816b26cc714b5f263489d53fe7d9c4b82ae0
AsifNawaz0129 <velocityscript360@gmail.com>
2021-05-02 14:24:16 +0500

    uncomment headless line

--- src/main/java/resources/base.java
+++ src/main/java/resources/base.java
@@ -42,7 +42,7 @@ public class base {
             ChromeOptions options = new ChromeOptions();
             options.addArguments("--no-sandbox");
             options.addArguments("--disable-dev-shm-usage");
-            // options.addArguments("--headless");
+            options.addArguments("--headless");
             driver = new ChromeDriver(options);
         } else if (browserName.equals("firefox")) {
             // Initialize Firefox browser
